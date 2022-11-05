c91d140568e1ea13b2a2dc6d30fb19aefda7361d
Domenic Betters <domenicbetters@yahoo.com>
2022-11-05 16:32:24 -0400

    cleanup complete.  variables all renamed to standard rubyt format with readability in mind

--- lib/gravity.rb
+++ lib/gravity.rb
@@ -1,69 +1,48 @@
-
-
 class Physics
 
-def initialize
-  @fallspeed = 0
-  $jump_speed = -40
-  @time = 0 
-  @time2 = 0
-  @floatytime = 0
-  @floaty = 1
-  
-  @groundfloor = $grounded
-  @velocity = 0
+  def initialize
+    $jump_speed = -40
+    @gravity_timer = 0 
+    @jump_timer = 0
+    @floaty_time = 0
+    @floaty = 1
+    @ground_floor = $grounded
+    @velocity = 0
+  end    
 
+  def gravity(thing) 
+    @gravity_timer = @gravity_timer + 0.15 if thing.y < @ground_floor
+    @gravity_timer = 0 if thing.y >= @ground_floor  
+    @velocity = (1 * @gravity_timer)*2 if @gravity_timer >= 1
 
-end
+    if (thing.y + @velocity) > @ground_floor
+      @velocity = (@ground_floor - thing.y)
+    end
+    
+    if thing.y <= @ground_floor
+      thing.y += (@velocity)
+    end
+  end
 
-
-  
-      
-
-    def gravity(thing) 
-        @time = @time + 0.15 if thing.y < @groundfloor
-        @time = 0 if thing.y >= @groundfloor  
-        
-
-        @velocity = (1 * @time)*2 if @time >= 1
-       
-        if (thing.y + @velocity) > @groundfloor
-          @velocity = (@groundfloor - thing.y)
-        end
-        if thing.y <= @groundfloor
-        thing.y += (@velocity)
-        
-        end
-        
-       
-        
-      
-      end
-
-    def jump(thing)
-      @time2 = @time2 + 0.01 if thing.y < @groundfloor
-      @time2 = 0 if thing.y >= @groundfloor
-      # $jump_speed = -30 if thing.y >= @groundfloor
-
-        
-      $jump_speed  =  ($jump_speed + @time2) if  $jump_speed <= 0
-      if ($jump_speed + @time2) > 0 
-        $jump_speed = 0
-      end
-     
-
-      
-      thing.y += $jump_speed
-      
-      
+  def jump(thing)
+    @jump_timer = @jump_timer + 0.01 if thing.y < @ground_floor
+    @jump_timer = 0 if thing.y >= @ground_floor 
+    $jump_speed  =  ($jump_speed + @jump_timer) if  $jump_speed <= 0
+    
+    if ($jump_speed + @jump_timer) > 0 
+      $jump_speed = 0
     end
 
-    def flying(thing2, updown, bounce, start)    
-      @floatytime += 1
-      thing2.x -= 7
-      thing2.y += updown if @floaty == start
-      thing2.y -= updown if @floaty == -start
-      @floaty = -@floaty if @floatytime %  bounce == 0
-    end
+    thing.y += $jump_speed
 
+  end
+
+  def flying(thing2, updown, bounce, start)    
+    @floaty_time += 1
+    thing2.x -= 7
+    thing2.y += updown if @floaty == start
+    thing2.y -= updown if @floaty == -start
+    @floaty = -@floaty if @floaty_time %  bounce == 0
+  end
+  
  end
